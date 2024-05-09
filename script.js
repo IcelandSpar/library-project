7d86d6689465dc43c199edfab1f4b45386044650
IcelandSpar <victorasielrodriguez@gmail.com>
2024-05-09 18:37:07 -0500

    Add loop to display each book on tablerow

--- script.js
+++ script.js
@@ -10,16 +10,58 @@ function Book(title, author, pages, read) {
       return `${this.title}, by ${this.author} is ${this.pages} pages long, and I have ${this.read} this book.`
     }
   }
-  const againstTheDay = new Book("Against The day", "Thomas Pynchon", 1200, "read");
+
+  function addBookToLibrary() {
+    const formContainer = document.querySelector('.formContainer');
+    const newForm = document.createElement("div");
+  
+    newForm.style.cssText = 'color: green;'
+    newForm.textContent = 'Hello World'
+    formContainer.appendChild(newForm)
+  
+  }
+
+
+  const againstTheDay = new Book("Against The day", "Thomas Pynchon", 1805, "read");
+  const theHobbit = new Book("The Hobbit", "J. R. R. Tolkien", 310, "not read");
+  const infiniteJest = new Book("Infinite Jest", "David Foster Wallace", 2005, "read");
+  const howToBeAlone = new Book("How to Be Alone", "Jonathan Franzen", 320, "read");
   
  
-  const myLibrary = [];
+  const myLibrary = [againstTheDay.info(), theHobbit.info(), infiniteJest.info(), howToBeAlone.info()];
+
+
+
 
   let looper = function() {
 
     for(i = 0; i < myLibrary.length; i++) {
         console.log(myLibrary[i]);
+        const table = document.querySelector(".table");
+        const tableRow = document.createElement("tr");
+        const tableData = document.createElement("td");
+        
+
+
+        tableData.textContent = myLibrary[i];
+        
+
+        table.appendChild(tableRow);
+        tableRow.appendChild(tableData);
+
     }
   }
 
-  
+  looper();
+
+
+
+
+
+
+  const newBookBtn = document.querySelector('.newBookBtn')
+
+  newBookBtn.addEventListener('click', () => {
+
+    
+  })
