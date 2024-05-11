a52350d7cd5163bc37000a23a7bf845a4bc55d10
IcelandSpar <victorasielrodriguez@gmail.com>
2024-05-11 03:37:33 -0500

    Add remove book button and change read status
    Included styling to table for clarity. Added an exit button for dialog pop up for form to close and allow access to page.

--- script.js
+++ script.js
@@ -28,10 +28,10 @@ function Book(title, author, pages, read) {
   const howToBeAlone = new Book("How to Be Alone", "Jonathan Franzen", 320, "read");
   
  
-  const myLibrary = [againstTheDay.info(), theHobbit.info(), infiniteJest.info(), howToBeAlone.info()];
-
-
-
+  const myLibrary = [againstTheDay.title, theHobbit.title, infiniteJest.title, howToBeAlone.title];
+  const myAuthors= [againstTheDay.author, theHobbit.author, infiniteJest.author, howToBeAlone.author];
+  const myPages = [againstTheDay.pages, theHobbit.pages, infiniteJest.pages, howToBeAlone.pages]
+  const readOrNot = [againstTheDay.read, theHobbit.read, infiniteJest.read, howToBeAlone.read]
 
   let looper = function() {
 
@@ -39,15 +39,72 @@ function Book(title, author, pages, read) {
         console.log(myLibrary[i]);
         const table = document.querySelector(".table");
         const tableRow = document.createElement("tr");
-        const tableData = document.createElement("td");
+
+        const tableDataTitles = document.createElement("th");
+        const tableDataAuthors = document.createElement("td");
+        const tableDataPages = document.createElement("td");
+        const tableDataReads = document.createElement("td");
         
 
 
-        tableData.textContent = myLibrary[i];
+        tableDataTitles.textContent = myLibrary[i];
         
+        
+
+
 
         table.appendChild(tableRow);
-        tableRow.appendChild(tableData);
+        tableRow.appendChild(tableDataTitles);
+
+        
+        tableRow.appendChild(tableDataAuthors);
+        tableDataAuthors.textContent = myAuthors[i];
+
+        tableRow.appendChild(tableDataPages);
+        tableDataPages.textContent = myPages[i];
+
+        tableRow.appendChild(tableDataReads);
+        tableDataReads.textContent = readOrNot[i];
+
+       
+        const tableDataBtnContainer = document.createElement("td");
+        const tableReadBtn = document.createElement("button");
+
+        tableRow.appendChild(tableDataBtnContainer);
+        tableDataBtnContainer.appendChild(tableReadBtn);
+        
+        tableReadBtn.textContent = "Change";
+        tableReadBtn.style.cssText = "padding: 12px 1ch; font-size: 1.5rem; font-family: 'Comic Neue'; border-radius: 10px;"
+    /////////////////////////
+        tableReadBtn.addEventListener("click", () => {
+          if (tableDataReads.textContent == "read") {
+            tableDataReads.textContent = "not read";
+          } else {
+            tableDataReads.textContent = "read";
+          }
+        //////////////////////////////
+        });
+
+
+        
+
+
+    const tableDataRemoveContainer = document.createElement("td");
+    const tableDataRemoveButton = document.createElement("button");
+    tableRow.appendChild(tableDataRemoveContainer);
+    tableDataRemoveContainer.appendChild(tableDataRemoveButton);
+    tableDataRemoveButton.textContent = "Remove Book";
+
+    tableDataRemoveButton.style.cssText = "padding: 12px 1ch; font-size: 1.5rem; font-family: 'Comic Neue'; border-radius: 10px;"
+
+    
+    
+    tableDataRemoveButton.addEventListener("click", () => {
+      table.removeChild(tableRow);
+    })
+
+
+
 
     }
   }
@@ -67,7 +124,11 @@ const dialog = document.querySelector(".dialog")
     form.reset();
   })
 
+const exitBtn = document.querySelector('.exitBtn');
 
+exitBtn.addEventListener("click", () => {
+  dialog.close();
+})
 
 
   const addBookBtn = document.querySelector("button[value=save]");
@@ -83,14 +144,75 @@ const dialog = document.querySelector(".dialog")
     const newBook = new Book(title.value, author.value, pages.value, read.value);
 
     const tableRow = document.createElement("tr");
-    const tableData = document.createElement("td");
+    const tableDataTitle = document.createElement("th");
     
     const table = document.querySelector(".table");
 
 
     table.appendChild(tableRow);
-    tableRow.appendChild(tableData);
-    tableData.textContent = newBook.info();
+    tableRow.appendChild(tableDataTitle);
+    tableDataTitle.textContent = newBook.title;
+
+    
+
+    const tableDataAuthor = document.createElement("td");
+
+    tableRow.appendChild(tableDataAuthor);
+    tableDataAuthor.textContent = newBook.author;
+
+
+    const tableDataPages = document.createElement("td");
+
+    tableRow.appendChild(tableDataPages);
+    tableDataPages.textContent = newBook.pages;
+
+    
+    const tableDataRead = document.createElement("td");
+    const tableDataBtnContainer = document.createElement("td");
+    const tableReadBtn = document.createElement("button");
+
+    tableRow.appendChild(tableDataRead);
+    
+
+    
+    tableDataRead.textContent = newBook.read;
+    tableRow.appendChild(tableDataBtnContainer);
+    tableDataBtnContainer.appendChild(tableReadBtn);
+    
+    tableReadBtn.textContent = "Change";
+    tableReadBtn.style.cssText = "padding: 12px 1ch; font-size: 1.5rem; font-family: 'Comic Neue'; border-radius: 10px;"
+/////////////////////////
+    tableReadBtn.addEventListener("click", () => {
+      if (tableDataRead.textContent == "read") {
+        tableDataRead.textContent = "not read";
+      } else {
+        tableDataRead.textContent = "read";
+      }
+    //////////////////////////////
+    })
+
+
+
+
+    const tableDataRemoveContainer = document.createElement("td");
+    const tableDataRemoveButton = document.createElement("button");
+    tableRow.appendChild(tableDataRemoveContainer);
+    tableDataRemoveContainer.appendChild(tableDataRemoveButton);
+    tableDataRemoveButton.textContent = "Remove Book";
+
+    tableDataRemoveButton.style.cssText = "padding: 12px 1ch; font-size: 1.5rem; font-family: 'Comic Neue'; border-radius: 10px;"
+
+    
+    
+    tableDataRemoveButton.addEventListener("click", () => {
+      table.removeChild(tableRow);
+    })
+
+
     
   })
+
+
+
+
 
